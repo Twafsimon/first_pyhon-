2a4850fb630456cb86a13b96b6c0d2719b688a1b
Twaf Simon <73712242+Twafsimon@users.noreply.github.com>
2021-10-10 15:53:50 +0300

    Create Try_pyhon.py

---
+++ Try_pyhon.py
@@ -0,0 +1,14 @@
+# this code show how you can write code in GitHub
+print("this is try python")
+import pandas as pd
+import matplotlib as mpt
+import maths
+a=10
+b=122
+c= a+b
+print('the sum of two numbers is : ',c)
+print('Subtruction is given be: ',b-c)
+print('Multiplication is :', a * b)
+print('division with decimal:',b/a)
+print(division with rounded: ', b//a)
+
